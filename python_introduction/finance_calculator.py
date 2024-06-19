0606280f1aee3f942c99819c33dd8f1c6d6af531
root <abbeyriunga12@gmail.com>
2024-06-19 12:54:44 -0700

    Added values to the file

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,13 +1,7 @@
 monthly_income = 5000
 monthly_expenses = 4000
-
 monthly_savings = monthly_income - monthly_expenses
-
 annual_interest_rate = 0.05
-
 projected_annual_savings = monthly_savings * 12 * (1 + annual_interest_rate)
-
 print(f"Your monthly savings are: ${monthly_savings:.2f}")
-
 print(f"Your projected annual savings after including interest are: ${projected_annual_savings:.2f}")
-
