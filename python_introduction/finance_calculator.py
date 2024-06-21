e6bf20d4ba4a25f47251ba6ecf135dd8677bfe3b
root <abbeyriunga12@gmail.com>
2024-06-21 12:55:32 -0700

    Removed the values and wrote prompts.

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,13 @@
-monthly_income = 5000
-monthly_expenses = 4000
+monthly_income = float(input("Enter your monthly income: "))
+
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+
 monthly_savings = monthly_income - monthly_expenses
+
 annual_interest_rate = 0.05
+
 projected_annual_savings = monthly_savings * 12 * (1 + annual_interest_rate)
+
 print(f"Your monthly savings are: ${monthly_savings:.2f}")
+
 print(f"Your projected annual savings after including interest are: ${projected_annual_savings:.2f}")
