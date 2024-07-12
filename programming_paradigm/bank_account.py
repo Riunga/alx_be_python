434a5e9f8a3824772a836ba27341e97fb6a7eaf2
Riunga <abbeyriunga12@gmail.com>
2024-07-12 15:23:41 +0300

    Done

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,14 +5,14 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.__account_balance += amount
-            print(f"Deposited: ${amount:.1f}")
+            print(f"Deposited: ${amount:.1f}".rstrip('0').rstrip('.'))
         else:
             print("Deposit amount must be positive.")
 
     def withdraw(self, amount):
         if 0 < amount <= self.__account_balance:
             self.__account_balance -= amount
-            print(f"Withdrew: ${amount:.1f}")
+            print(f"Withdrew: ${amount:.1f}".rstrip('0').rstrip('.'))
             return True
         elif amount > self.__account_balance:
             print("Insufficient funds.")
@@ -22,4 +22,5 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.__account_balance:.1f}")
+        print(f"Current Balance: ${self.__account_balance:.1f}".rstrip('0').rstrip('.'))
+
