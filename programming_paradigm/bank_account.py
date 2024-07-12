3cbbcab983b1e879e9918008fd158adf92ad7b14
Riunga <abbeyriunga12@gmail.com>
2024-07-12 21:13:17 +0300

    "Updated display_balance method to format balance with two decimal places instead of one."

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -12,4 +12,4 @@ class BankAccount:
             return True
          return False
    def display_balance(self):
-        print(f"Current Balance: ${self.__account_balance:.1f}".rstrip('0').rstrip('.'))
+        print(f"Current Balance: ${self.__account_balance:.2f}".rstrip('0').rstrip('.'))
